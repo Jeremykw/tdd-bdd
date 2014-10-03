28c06cfd43fc45a23a64a6df577207c96eed5c64
Jeremy <jeremy@kwmassage.com>
2014-10-03 14:50:40 -0300

    rn similar_director.rb to _spec.rb

--- spec/controllers/similar_director_spec.rb
+++ spec/controllers/similar_director_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe MoviesController do 
+
 	before do
 		FactoryGirl.create(:movie, 
 			id: 1,
 		    title: "Star Wars",
 		    rating: "PG",
-		    director: "Ridley Scott",
+		    director: "George Lucas",
 		    release_date: "1977-05-25"
 		    )
 		FactoryGirl.create(:movie, 
@@ -16,25 +17,22 @@ describe MoviesController do
 		    director: "Ridley Scott",
 		    release_date: "1977-05-25"
 		    )
+		FactoryGirl.create(:movie, 
+			id: 3,
+		    title: "Alien ",
+		    rating: "R",
+		    director: "",
+		    release_date: "1977-05-25"
+		    )
+		FactoryGirl.create(:movie, 
+			id: 4,
+		    title: "THX-1138",
+		    rating: "R",
+		    director: "George Lucas",
+		    release_date: "1971-03-1"
+		    )
 	end
-=begin
 
-  factory :movies do
-    id "3"
-    title "Alien "
-    rating "R"
-    director ""
-    release_date "1979-05-25"
-  end
-
-  factory :movies do
-    id "4"
-    title "THX-1138"
-    rating "R"
-    director "George Lucas"
-    release_date "1971-03-11"
-  end
-=end
 	describe 'create movie' do
 		it 'creates a movie' do
 			lambda{
@@ -47,15 +45,12 @@ describe MoviesController do
 			@movie.director.should be_nil
 		end
 	end
+
 	describe 'search by director' do
 		describe MoviesController do 
-
 			it 'calls #search_director' do
 				movie = Movie.find(1)
-				#movie.id.should == 1
 				movie.title.should == 'Star Wars'
-				#Movie.should_receive(:find_all_by_director).with('George Lucas')
-				#post :show, {:search_director => 'George Lucas'}
 			end
 		end
 	end
